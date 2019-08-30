70a9d226064d2d750c7395471d5a2f4ac1de6801
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-08-30 09:19:27 -0600

    initial commit

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,49 @@
 class ChartsController < ApplicationController
   def index
+    @charts = Chart.all
   end
 
   def new
+    @chart = Chart.new
+    #render partial: 'form'
+  end
+  
+  def show
+    @chart = Chart.find(params[:id])
   end
 
-  def show
+  def create
+    @chart = Chart.new(chart_params)
+
+    if @chart.save
+      redirect_to charts_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @chart = Chart.find(params[:id])
   end
+
+  def update
+    @chart = Chart.find(params[:id])
+
+    if @chart.update(chart_params)
+      redirect_to chart_path
+    else 
+      render :edit  
+    end
+  end
+
+  def destroy
+    Chart.find(params[:id]).destroy
+    redirect_to charts_path
+  end
+
+  private
+  def chart_params
+  params.require(:chart).permit(:attr, :title, :genre)
+  end
+
 end
